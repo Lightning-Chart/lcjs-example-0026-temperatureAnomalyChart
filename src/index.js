6163f94b5459967536a20d82198495034dacae8e
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:10:17 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const lcjs = require('@arction/lcjs')
 
 const { lightningChart, AxisTickStrategies, UIElementBuilders, UIOrigins, ImageFill, emptyLine, ImageFitMode, emptyFill, Themes } = lcjs
 
-const chart = lightningChart()
+const chart = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
     .ChartXY({
         theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     })
